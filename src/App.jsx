83ae10497e4c961bacf93d48953cfacce269d165
Maboku1111 <mabokuseimela6@gmail.com>
2025-03-24 09:58:01 +0200

    imported necessary library

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
+/* App.jsx */
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 
 function App() {
   return (
-    <div className="bg-gray-50 dark:bg-gray-800 min-h-screen">
+    <div className="container">
       <Header />
       <Hero />
-
-      <div className="bg-red-500 text-blue p-4">
-        If you see this, Tailwind is working!
-      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,20 @@
-import { SpinningText } from "@/components/magicui/spinning-text";
+/* Header.jsx */
+import { AuroraText } from "@/components/magicui/aurora-text";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center px-8 py-4 bg-white dark:bg-gray-900 shadow-md">
-      {/* Logo */}
-      <div className="text-xl font-bold">
-        <SpinningText reverse className="text-4xl" duration={4} radius={6}>
-          learn more • earn more • grow more •
-        </SpinningText>
+    <header>
+      <div className="logo">
+        Ezrah <AuroraText>Codes</AuroraText>
       </div>
 
-      {/* Navbar */}
-      <nav className="hidden md:flex space-x-6 text-gray-700 dark:text-white">
-        <a href="#" className="hover:text-blue-600">Home</a>
-        <a href="#" className="hover:text-blue-600">About</a>
-        <a href="#" className="hover:text-blue-600">Projects</a>
-        <a href="#" className="hover:text-blue-600">Blog</a>
-        <a href="#" className="hover:text-blue-600">Contact Me</a>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Blog</a>
+        <a href="#">Contact Me</a>
       </nav>
-
-      {/* Placeholder for Mobile Menu (we’ll add later) */}
     </header>
   );
 };
