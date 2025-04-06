1cdb84f1f74b9d7ca31404bad50a38e648a3cb40
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2025-04-06 08:59:13 +0200

    i have no idea what are the latest changes but i must just push this work, chief

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,8 +3,8 @@ import ScrollBasedVelocityDemo from "./Scroll";
 
 function Hero() {
   return (
-    <section className="min-h-screen relative -top-4 -left-4">
-      <div className="relative h-16 w-16 mt-28 w-full max-w-lg float-end">
+    <section className="min-h-screen relative -top-4 -left-4 mb-96">
+      <div className="relative h-16 w-16 mt-28 max-w-lg float-end">
         <div className="absolute top-0 -left-4 w-72 h-72 bg-pink-700 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
         <div className="absolute top-0 -right-4 w-72 h-72 bg-blue-600 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
         <div className="absolute -bottom-8 left-20 w-72 h-72 bg-indigo-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000"></div>
